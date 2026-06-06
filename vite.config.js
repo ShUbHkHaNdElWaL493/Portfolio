6198003e6f6bddafd0521ee14649e5ac964595d8
Shubh Khandelwal <shubh4664@gmail.com>
2026-06-07 04:43:15 +0530

    Added support for web hosting using gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/',
   // Vite can tree-shake Three.js quite well with this hint
   optimizeDeps: {
     include: ['three', 'gsap'],
